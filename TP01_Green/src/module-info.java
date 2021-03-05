7fd2b23dc7745603b770f4eedab7ad0c8cdb4160
srmarrs <73143017+srmarrs@users.noreply.github.com>
2021-03-04 23:49:00 -0600

    Initial Commit

---
+++ TP01_Green/src/module-info.java
@@ -0,0 +1,6 @@
+module TP.Green {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens sample;
+}
---
+++ TP01_Green/src/sample/Controller.java
@@ -0,0 +1,45 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML Button btnGrayScale;
+    @FXML Button btnSepia;
+    @FXML Button btnSurprise;
+    @FXML ImageView imgBefore;
+    @FXML ImageView imgAfter;
+
+    //Import Image with ImagePicker there's an example of it on D2l
+
+    //Convert before image to Gray Scale in the After Image
+    public void convertGrayScale(ActionEvent actionEvent) {
+    }
+
+    //Convert Image to Sepia in the After Image
+    public void convertSepia(ActionEvent actionEvent) {
+    }
+
+    //Convert Image to whatever you want in the After Image
+    public void convertSurprise(ActionEvent actionEvent) {
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //Set Images
+
+        /*
+        I assume that setting the images needs to be done here
+        If not just take out the implements Initializable thing
+         */
+    }
+
+
+}
